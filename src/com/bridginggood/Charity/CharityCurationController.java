8962afdf9132225c0a8251a900a5177d6fb2d027
Junsung <js.lim@bridginggood.com>
2012-02-26 20:31:45 +0900

    added fragment for pageradapter

--- src/com/bridginggood/Charity/CharityCurationController.java
+++ src/com/bridginggood/Charity/CharityCurationController.java
@@ -1,22 +1,45 @@
 package com.bridginggood.Charity;
 
-import android.app.Activity;
+import java.util.List;
+import java.util.Vector;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import com.bridginggood.R;
 import com.viewpagerindicator.CirclePageIndicator;
 
-public class CharityCurationController extends Activity{
+public class CharityCurationController extends FragmentActivity{
+	/** maintains the pager adapter*/
+	private PagerAdapter mPagerAdapter;
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+	 */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.charity_layout);
-		
-		CharityPagerAdapter adapter = new CharityPagerAdapter( this );
-		ViewPager pager = (ViewPager)findViewById( R.id.viewpagerCharity );
-		pager.setAdapter( adapter );
-		
+		super.setContentView(R.layout.charity_layout);
+		//initialsie the pager
+		this.initialisePaging();
+	}
+
+	/**
+	 * Initialise the fragments to be paged
+	 */
+	private void initialisePaging() {
+
+		List<Fragment> fragments = new Vector<Fragment>();
+		fragments.add(Fragment.instantiate(this, CharityPage1.class.getName()));
+		fragments.add(Fragment.instantiate(this, CharityPage2.class.getName()));
+		fragments.add(Fragment.instantiate(this, CharityPage3.class.getName()));
+		this.mPagerAdapter  = new CharityPagerAdapter(super.getSupportFragmentManager(), fragments);
+		//
+		ViewPager pager = (ViewPager)super.findViewById(R.id.viewpagerCharity);
+		pager.setAdapter(this.mPagerAdapter);
+
 		//Bind the title indicator to the adapter
 		CirclePageIndicator circleIndicator = (CirclePageIndicator)findViewById(R.id.indicatorViewPagerCharity);
 		circleIndicator.setViewPager(pager);
