0cd9f9c3b7730ce1205830812bcb6c3f4d4d5522
jslim <jslim@ljs-PC.inca.local>
2012-03-22 17:50:54 +0900

    need to make business detail page.

--- src/com/bridginggood/User/UserActivity.java
+++ src/com/bridginggood/User/UserActivity.java
@@ -1,36 +1,36 @@
 package com.bridginggood.User;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.bridginggood.R;
-import com.bridginggood.UserInfo;
-import com.bridginggood.UserInfoStore;
 
 public class UserActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		TextView txtHello = (TextView) findViewById(R.id.txtHello);
-		txtHello.setText("Hello "+UserInfo.getUserFirstName()+" "+UserInfo.getUserLastName()+"\n"+
-				UserInfo.getUserEmail()+" | Type: "+UserInfo.getUserType()+" | DeviceId: "+UserInfo.getDeviceId()
-			+" | c2dm: "+UserInfo.getC2DMRegistrationId());
+		setContentView(R.layout.profile_main_layout);
 
-		initButton();
+		initButtons();
 	}
 
-	private void initButton(){
-		Button btnGoBizMap = (Button) findViewById(R.id.btnTestButton);
-		btnGoBizMap.setOnClickListener(new OnClickListener(){
-			public void onClick(View v){
-				Log.d("BG", "Button Clicked");
-				UserInfoStore.clearSession(getApplicationContext());
+	private void initButtons(){
+		
+		/** Footer buttons */
+		//Today
+		//Month
+		//Week
+		
+		//Preferences
+		Button btnPreferences = (Button)findViewById(R.id.profile_btn_preferences);
+		btnPreferences.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent().setClass(getParent(), UserPreferencesActivity.class));	
 			}
 		});
 	}
